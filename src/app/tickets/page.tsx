f7525ed30367803f9a12472afdcce9491b0b6a31
Szymon Wiatr <szymon.wiatr@me.com>
2024-11-24 20:28:01 +0100

    TS

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,12 +2,19 @@ import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 import Link from "next/link";
 
+const TICKET_ICONS = {
+  OPEN: "O",
+  IN_PROGRESS: ">",
+  DONE: "X"
+}
+
 const TicketsPage = () => {
   return (
     <div>
       {initialTickets.map((ticket) => {
         return (
           <div key={ticket.id}>
+            <div>{TICKET_ICONS[ticket.status]}</div>
             <h2 className="text-lg">{ticket.title}</h2>
             <Link href={ticketPath(ticket.id)} className="text-sm underline">
               View
