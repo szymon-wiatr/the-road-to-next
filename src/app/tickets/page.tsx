3242c914b8a51ce983f50afc459ad5427446d4a3
Szymon Wiatr <szymon.wiatr@me.com>
2024-11-24 21:04:11 +0100

    conditional jsx

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 import Link from "next/link";
@@ -26,7 +27,13 @@ const TicketsPage = () => {
             >
               <div>{TICKET_ICONS[ticket.status]}</div>
               <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
-              <p className="text-sm text-slate-500 truncate">{ticket.content}</p>
+              <p
+                className={clsx("text-sm text-slate-500 truncate", {
+                  "line-through": ticket.status === "DONE",
+                })}
+              >
+                {ticket.content}
+              </p>
               <Link href={ticketPath(ticket.id)} className="text-sm underline">
                 View
               </Link>
