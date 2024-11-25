11e3199df6b3d764e9d18b4d31c8917a8d4f0d32
Szymon Wiatr <szymon.wiatr@me.com>
2024-11-25 14:29:45 +0100

    Streaming Suspense

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,19 +1,14 @@
+import { Suspense } from "react";
 import { Heading } from "@/components/heading";
-import { TicketItem } from "@/features/ticket/components/ticket-item";
-import { getTickets } from "@/features/queries/get-tickets";
+import { TicketList } from "@/features/ticket/components/ticket-list";
 
-const TicketsPage = async () => {
-  const tickets = await getTickets();
-
+const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
-
-      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
-        {tickets.map((ticket) => {
-          return <TicketItem key={ticket.id} ticket={ticket} />;
-        })}
-      </div>
+      <Suspense>
+        <TicketList />
+      </Suspense>
     </div>
   );
 };
