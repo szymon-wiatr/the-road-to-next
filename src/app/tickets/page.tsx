159f2d0ab94763edcca49d7047e37b5245fdc898
Szymon Wiatr <szymon.wiatr@me.com>
2024-11-25 14:41:47 +0100

    Spinner

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from "react";
 import { Heading } from "@/components/heading";
 import { TicketList } from "@/features/ticket/components/ticket-list";
+import { Spinner } from "@/components/spinner";
 
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
-      <Suspense>
+      <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
     </div>
