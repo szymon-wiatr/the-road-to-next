5e99a173e81929d3fad39449342a44fb73f73ae2
Szymon Wiatr <szymon.wiatr@me.com>
2024-11-25 11:35:58 +0100

    not found placeholder

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,4 +1,8 @@
+import { Placeholder } from "@/components/placeholder";
+import { Button } from "@/components/ui/button";
 import { initialTickets } from "@/data";
+import { ticketsPath } from "@/paths";
+import Link from "next/link";
 
 type TicketPageProps = {
   params: Promise<{
@@ -10,8 +14,17 @@ const TicketPage = async ({ params }: TicketPageProps) => {
   const { ticketId } = await params;
   const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
 
-  if(!ticket) {
-    return <div>Ticket not found</div>
+  if (!ticket) {
+    return (
+      <Placeholder
+        label="Ticket not found"
+        button={
+          <Button asChild variant="outline">
+            <Link href={ticketsPath()}>Go to Tickets</Link>
+          </Button>
+        }
+      />
+    );
   }
 
   return (
