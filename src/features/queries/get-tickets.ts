89b295de62d37e74f2dc1306068c2da759125a13
Szymon Wiatr <szymon.wiatr@me.com>
2024-11-28 14:52:14 +0100

    quering database with prisma

--- src/features/queries/get-tickets.ts
+++ src/features/queries/get-tickets.ts
@@ -1,10 +1,10 @@
-import { initialTickets } from "@/data";
 import { Ticket } from "../types";
+import prisma from "@/lib/prisma";
 
 export const getTickets = async (): Promise<Ticket[]> => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  return new Promise((resolve) => {
-    resolve(initialTickets);
+  return await prisma.ticket.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
   });
 };
