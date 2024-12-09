72b3af764918da26388f8f8174d57b047af6e208
Szymon Wiatr <szymon.wiatr@me.com>
2024-12-09 14:58:29 +0100

    Error Handling

---
+++ src/components/form/utils/to-action-state.ts
@@ -0,0 +1,19 @@
+import { ZodError } from "zod";
+
+export type ActionState = { message: string; payload?: FormData };
+
+export const fromErrorToActionState = (
+  error: unknown,
+  formData: FormData
+): ActionState => {
+  if (error instanceof ZodError) {
+    return {
+      message: error.errors[0].message,
+      payload: formData,
+    };
+  } else if (error instanceof Error) {
+    return { message: error.message, payload: formData };
+  } else {
+    return { message: "An unknown error occured", payload: formData };
+  }
+};
--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { ActionState, fromErrorToActionState } from "@/components/form/utils/to-action-state";
 import prisma from "@/lib/prisma";
 import { ticketPath, ticketsPath } from "@/paths";
 import { revalidatePath } from "next/cache";
@@ -13,7 +14,7 @@ const upsertTicketSchema = z.object({
 
 export const upsertTicket = async (
   id: string | undefined,
-  _actionState: { message: string, payload?: FormData },
+  _actionState: ActionState,
   formData: FormData
 ) => {
   try {
@@ -29,8 +30,8 @@ export const upsertTicket = async (
       update: data,
       create: data,
     });
-  } catch (_error) {
-    return { message: "Something went wrong", payload: formData };
+  } catch (error) {
+    return fromErrorToActionState(error, formData)
   }
   revalidatePath(ticketsPath());
 
