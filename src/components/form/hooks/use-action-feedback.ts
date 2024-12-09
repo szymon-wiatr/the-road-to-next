806ce1151f5559af3c86be8fc255b56d0c8d33f2
Szymon Wiatr <szymon.wiatr@me.com>
2024-12-09 17:01:33 +0100

    Debugging

--- src/components/form/hooks/use-action-feedback.ts
+++ src/components/form/hooks/use-action-feedback.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { ActionState } from "../utils/to-action-state";
 
 type OnArgs = { actionState: ActionState };
@@ -12,14 +12,22 @@ const useActionFeedback = (
   actionState: ActionState,
   options: UseActionFeedbackOptions
 ) => {
+  const prevTimestamp = useRef(actionState.timestamp);
+  const isUpdate = prevTimestamp.current !== actionState.timestamp;
+
   useEffect(() => {
+    if (!isUpdate) return;
+
     if (actionState.status === "SUCCESS") {
       options.onSuccess?.({ actionState });
     }
+    
     if (actionState.status === "ERROR") {
       options.onError?.({ actionState });
     }
-  }, [actionState, options]);
+
+    prevTimestamp.current = actionState.timestamp;
+  }, [isUpdate, actionState, options]);
 };
 
 export { useActionFeedback };
