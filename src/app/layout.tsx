b55a7c52c792bda4dea8f6121d3a31c798a5787f
szymon.wiatr@wyden.io <szymon.wiatr@me.com>
2025-01-02 21:44:19 +0100

    Layouts vs Templates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { Header } from "@/components/header";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-import { RedirectToast } from "@/components/redirect-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -46,7 +45,6 @@ export default function RootLayout({
             {children}
           </main>
           <Toaster expand />
-          <RedirectToast />
         </ThemeProvider>
       </body>
     </html>
