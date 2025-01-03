eca8e53446971ada32e8799e885a2a803f5ce9a2
szymon.wiatr@wyden.io <szymon.wiatr@me.com>
2025-01-03 16:57:07 +0100

    Update toast feedback

--- src/features/ticket/actions/update-ticket-status.ts
+++ src/features/ticket/actions/update-ticket-status.ts
@@ -1,12 +1,17 @@
 "use server";
 
-import { fromErrorToActionState, toActionState } from "@/components/form/utils/to-action-state";
+import {
+  fromErrorToActionState,
+  toActionState,
+} from "@/components/form/utils/to-action-state";
 import prisma from "@/lib/prisma";
 import { ticketsPath } from "@/paths";
 import { TicketStatus } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
 export const updateTicketStatus = async (id: string, status: TicketStatus) => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
   try {
     await prisma.ticket.update({
       where: { id },
