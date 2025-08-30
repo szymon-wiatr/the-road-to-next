45b03828068f32f88af9a5270db5907cfa5518ca
Szymon Wiatr <szymon.wiatr@me.com>
2025-08-30 16:44:55 +0200

    Advanced Navigation: Authenticated Sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,10 +71,19 @@ export default {
             transform: "translateY(0)",
           },
         },
+        "sidebar-from-left": {
+          from: {
+            transform: "translateX(-100%)",
+          },
+          to: {
+            transform: "translateX(0)",
+          },
+        },
       },
       animation: {
         "fade-in-from-top": "fade-in-from-top 0.5s ease-out",
         "header-from-top": "header-from-top 0.5s ease-out",
+        "sidebar-from-left": "sidebar-from-left 0.5s ease-out",
       },
       borderRadius: {
         lg: "var(--radius)",
