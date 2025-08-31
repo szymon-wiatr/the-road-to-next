3fc869c06361694ca969ba304ace4860e7d21db6
Szymon Wiatr <szymon.wiatr@me.com>
2025-08-31 10:21:11 +0200

    fix build

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -1,6 +1,6 @@
 import prisma from "@/lib/prisma";
 
-export const getTickets = async (userId: string | undefined) => {
+export const getTickets = async (userId?: string | undefined) => {
   return await prisma.ticket.findMany({
     where: {
       userId,
