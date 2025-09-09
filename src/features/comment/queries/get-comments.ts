136f93a8c46e0b769b9131c30ea4f2da7d0770d6
Szymon Wiatr <szymon.wiatr@me.com>
2025-09-09 06:48:42 +0200

    Cursor-Based Pagination: Timestamp

--- src/features/comment/queries/get-comments.ts
+++ src/features/comment/queries/get-comments.ts
@@ -4,20 +4,21 @@ import { getAuth } from "@/features/auth/queries/get-auth";
 import { isOwner } from "@/features/auth/utils/is-owner";
 import prisma from "@/lib/prisma";
 
-export const getComments = async (ticketId: string, offset?: number) => {
+export const getComments = async (ticketId: string, cursor?: number) => {
   const { user } = await getAuth();
 
   const where = {
     ticketId,
+    createdAt: {
+      lt: cursor ? new Date(cursor) : undefined,
+    },
   };
 
-  const skip = offset ?? 0;
   const take = 2;
 
   const [comments, count] = await prisma.$transaction([
     prisma.comment.findMany({
       where,
-      skip,
       take,
       include: {
         user: {
@@ -42,7 +43,8 @@ export const getComments = async (ticketId: string, offset?: number) => {
     })),
     metadata: {
       count,
-      hasNextPage: count > skip + take,
+      hasNextPage: true, // Implement hasNextPage
+      cursor: comments.at(-1)?.createdAt.valueOf(),
     },
   };
 };
