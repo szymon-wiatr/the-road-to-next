62b3f1ebea4c4d47eeeaa2099bbfae15efc5615d
Szymon Wiatr <szymon.wiatr@me.com>
2025-09-29 09:17:33 +0200

    Manage Organization

--- src/features/organization/components/organization-list.tsx
+++ src/features/organization/components/organization-list.tsx
@@ -1,25 +1,89 @@
 import { format } from "date-fns";
+import {
+  LucideArrowLeftRight,
+  LucideArrowUpRightFromSquare,
+  LucidePen,
+  LucideTrash,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { getOrganizationsByUser } from "../queries/get-organizations-by-user";
 
 const OrganizationList = async () => {
   const organizations = await getOrganizationsByUser();
 
   return (
-    <div className="animate-fade-from-top">
-      {organizations.map((organization) => (
-        <div key={organization.id}>
-          <div>Name: {organization.name}</div>
-          <div>
-            Joined at:{" "}
-            {format(
-              organization.membershipByUser?.joinedAt,
-              "yyyy-MM-dd, HH:mm"
-            )}
-          </div>
-          <div>Memberships: {organization._count.memberships}</div>
-        </div>
-      ))}
-    </div>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>ID</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Joined At</TableHead>
+          <TableHead>Members</TableHead>
+          <TableHead />
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {organizations.map((organization) => {
+          const switchButton = (
+            <Button variant="outline" size="icon">
+              <LucideArrowLeftRight className="w-4 h-4" />
+            </Button>
+          );
+
+          const detailButton = (
+            <Button variant="outline" size="icon">
+              <LucideArrowUpRightFromSquare className="w-4 h-4" />
+            </Button>
+          );
+
+          const editButton = (
+            <Button variant="outline" size="icon">
+              <LucidePen className="w-4 h-4" />
+            </Button>
+          );
+
+          const deleteButton = (
+            <Button variant="destructive" size="icon">
+              <LucideTrash className="w-4 h-4" />
+            </Button>
+          );
+
+          const buttons = (
+            <>
+              {switchButton}
+              {detailButton}
+              {editButton}
+              {deleteButton}
+            </>
+          );
+
+          return (
+            <TableRow key={organization.id}>
+              <TableCell>{organization.id}</TableCell>
+              <TableCell>{organization.name}</TableCell>
+              <TableCell>
+                {format(
+                  organization.membershipByUser.joinedAt,
+                  "yyyy-MM-dd, HH:mm"
+                )}
+              </TableCell>
+              <TableCell>{organization._count.memberships}</TableCell>
+              <TableCell className="flex justify-end gap-x-2">
+                {buttons}
+              </TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
   );
 };
 
