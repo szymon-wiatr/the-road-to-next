8599f28b90785da26673d27b16a906502c9d1940
Szymon Wiatr <szymon.wiatr@me.com>
2025-09-29 08:51:37 +0200

    Require Organization during Onboarding

---
+++ src/features/organization/actions/create-organization.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { setCookieByKey } from "@/actions/cookies";
+import {
+  ActionState,
+  fromErrorToActionState,
+} from "@/components/form/utils/to-action-state";
+import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
+import { prisma } from "@/lib/prisma";
+import { ticketsPath } from "@/paths";
+
+const createOrganizationSchema = z.object({
+  name: z.string().min(1).max(191),
+});
+
+export const createOrganization = async (
+  _actionState: ActionState,
+  formData: FormData
+) => {
+  const { user } = await getAuthOrRedirect({
+    checkOrganization: false,
+  });
+
+  try {
+    const data = createOrganizationSchema.parse({
+      name: formData.get("name"),
+    });
+
+    await prisma.organization.create({
+      data: {
+        ...data,
+        memberships: {
+          create: {
+            userId: user.id,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    return fromErrorToActionState(error);
+  }
+
+  await setCookieByKey("toast", "Organization created");
+  redirect(ticketsPath());
+};
